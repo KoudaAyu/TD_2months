ede90ef95076b82f1b05bf37aee875e5c704011e
AyuKouda <k024g0061@g.neec.ac.jp>
2026-03-09 10:29:55 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <Windows.h>
+#include"KamataEngine.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+	//エンジンの初期化
+	KamataEngine::Initialize();
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	while (true)
+	{
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+
+		dxCommon->PreDraw();
+
+		dxCommon->PostDraw();
+	}
+
+	KamataEngine::Finalize();
+	return 0;
+}
